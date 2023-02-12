0317e2cf0907daf0fd0aa98673cda60a39ce172a
whoshi <whoshi@v004.vaio.ne.jp>
2023-02-12 19:16:19 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+$(function () 
+{
+    
+    // 送信
+    $('form').submit(function () 
+    {
+      
+      var speach1 = document.getElementById("input_speach1").value;
+      
+      if(speach1 == '')
+      {
+	 speach1 = "nodata";     
+      }
+      
+      
+    
+      
+      //入力チェック  
+      if(speach1 === 'nodata')
+      {
+	             alert('入力が何もありません');
+                     return false;	   
+      }
+     
+      
+      var inputdata = "【発話内容1】" + "\n" + speach1 + "\n" + "【アクション】" + "\n" + robohonaction +  "\n" + "【ソング】" + "\n" + robohonsong;
+      inputdata = inputdata  + "\n" + "【ダンス】" + "\n" + robohondance + "\n"　+ "【発話内容2】" + "\n" + speach2 + "【翻訳】" + "\n" + transrate ;	    
+      
+     //GAS側Postイベントエントリ
+      var url = 'https://script.google.com/macros/s/AKfycbyZCPemsZEO01buDDevsEWfs8LbvTIIJdGBHMyNi-HW9l9sK4E/exec'
+      
+      liff.getProfile().then
+      (profile => 
+        {
+                 // プロフィール名
+                 const name = profile.displayName
+                 var JSONdata = {
+	                                 speach1:speach1
+                                 };
+            
+                 //スピナー表示
+                 //インジケータ表示
+                 // Loading 画像を表示
+                 dispLoading("ロボホンに送信中...");
+             
+                 $.post(url,
+                        JSONdata,
+                        function(dt)
+                        {
+	                   //インジケータ除去
+	                   // Loading 画像を消す
+                           removeLoading();
+	                   if(dt.message == 'success!')
+	                   {
+                               sendText(dt.message);//To LINE 送信
+	                       liff.closeWindow(); 
+	                   }else
+	                   {
+		                     window.alert("LIFFアプリでエラーが発生");  
+                           }
+	                }
+                 );
+     
+        }
+      );
+      
+      return false;
+    });
+});
